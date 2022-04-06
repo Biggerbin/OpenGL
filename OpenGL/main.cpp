6adc94a27dbe4052c9e505229c27794353699cb9
Biggerbin <75824696+Biggerbin@users.noreply.github.com>
2022-04-06 23:24:46 +0800

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by 赵赵安安 on 2022/4/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
