8d2e7c52f88d4565229e15e831bc55211b898f4c
Biggerbin <75824696+Biggerbin@users.noreply.github.com>
2022-04-13 21:56:52 +0800

    漏了的索引缓冲

---
+++ OpenGL/src/IndexBuffer.cpp
@@ -0,0 +1,30 @@
+//
+//  IndexBuffer.cpp
+//  OpenGL
+//
+//  Created by 赵赵安安 on 2022/4/13.
+//
+
+#include "IndexBuffer.hpp"
+#include "Renderer.hpp"
+
+IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count): m_Count(count) {
+    
+    assert(sizeof(unsigned int) == sizeof(GLuint));
+    
+    GLCaLL(glGenBuffers(1, &m_RendererID));
+    GLCaLL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+    GLCaLL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int ), data, GL_STATIC_DRAW));
+}
+
+void IndexBuffer::Bind() const{
+    GLCaLL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+}
+
+void IndexBuffer::unBinde() const{
+    GLCaLL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+}
+
+IndexBuffer::~IndexBuffer() { 
+    GLCaLL(glDeleteBuffers(1, &m_RendererID));
+}
